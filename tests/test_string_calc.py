eefc33d9fcff3cb64fd7d8e4d244393dfe54f841
Prince Mathur <37772855+princemathur4@users.noreply.github.com>
2025-03-20 23:25:47 +0530

    Revert "Adds test to validate if numbers param is only a string"
    This reverts commit 9937372f4e8325605f5c97647ef6ff857cd78e1f.

--- tests/test_string_calc.py
+++ tests/test_string_calc.py
@@ -1,12 +1,6 @@
-import pytest
 from string_calc import StringCalc
 
 class TestStringCalc:
-    def test_add_method_existance(self):
+    def test_add(self):
         calc_obj = StringCalc()
         assert hasattr(calc_obj, "add")
-
-    def test_add_non_string_input(self):
-        calc_obj = StringCalc()
-        with pytest.raises(Exception):
-            calc_obj.add(1)
