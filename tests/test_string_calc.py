9937372f4e8325605f5c97647ef6ff857cd78e1f
princemathur4 <princemathur.mathur4@gmail.com>
2025-03-20 21:45:25 +0530

    Adds test to validate if numbers param is only a string

--- tests/test_string_calc.py
+++ tests/test_string_calc.py
@@ -1,6 +1,12 @@
+import pytest
 from string_calc import StringCalc
 
 class TestStringCalc:
-    def test_add(self):
+    def test_add_method_existance(self):
         calc_obj = StringCalc()
         assert hasattr(calc_obj, "add")
+
+    def test_add_non_string_input(self):
+        calc_obj = StringCalc()
+        with pytest.raises(Exception):
+            calc_obj.add(1)
